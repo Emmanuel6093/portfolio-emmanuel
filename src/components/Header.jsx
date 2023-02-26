844d63ae44770eda97e581b0b76b5df72da649f6
Emmanuel6093 <elorenzo6093@gmail.com>
2023-02-26 16:35:25 -0500

    adding dark/light mode

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { MdNightsStay, MdWbSunny } from "react-icons/md";
+
+const Header = () => {
+  return (
+    <header className="bg-white dark:bg-gray-900 text-gray-900 dark:text-white">
+      <nav className="flex justify-between items-center p-5">
+        <h1 className="text-lg">EMMANUEL</h1>
+
+        <div>
+          <MdNightsStay className="text-2xl cursor-pointer" />
+          <MdWbSunny className="text-2xl cursor-pointer" />
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
