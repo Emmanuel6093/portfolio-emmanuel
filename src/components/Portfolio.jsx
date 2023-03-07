c1bfdc19012a938b888983307b0ee6618c5c4a8a
Emmanuel6093 <elorenzo6093@gmail.com>
2023-03-07 18:04:32 -0500

    setting up portfolio section

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
 import Section from './common/Section'
 
+import { FaGithub, FaExternalLinkAlt } from 'react-icons/fa'
+
 const Portfolio = () => {
+
+    const projects = [ 
+        {
+            id: 1, 
+            // image: , 
+            title: 'Best App', 
+            github: 'https://github.com',
+            demo: "https://google.com"
+        },
+    ];
+
   return (
     <Section 
-    title='Portfolio 🗒️' 
-    subtitle='These are all the projects that I have worked on.'
-    ></Section>
+        title='Portfolio 🗒️' 
+        subtitle='These are all the projects that I have worked on.'
+        >
+    </Section>
   )
-}
+};
 
 export default Portfolio
