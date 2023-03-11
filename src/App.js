d58cd211723d1baf1828c9aa637ae6d1a6568447
Emmanuel6093 <elorenzo6093@gmail.com>
2023-03-11 17:07:34 -0500

    minor changed on appJs

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import { useState } from "react";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Portfolio from "./components/Portfolio";
-import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import Contact from './components/Contact';
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
